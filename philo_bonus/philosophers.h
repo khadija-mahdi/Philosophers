5b81a590994555501531a412714de3d5281593a4
khadija-mahdi <khadijamahdi6@gmail.com>
2023-05-03 17:29:00 +0100

    the dirctorys and makefiles , file headers :) the start of project

---
+++ philo_bonus/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmahdi <kmahdi@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/29 16:14:50 by kmahdi            #+#    #+#             */
+/*   Updated: 2023/04/29 16:24:14 by kmahdi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
