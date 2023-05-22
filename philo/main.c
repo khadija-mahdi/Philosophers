ce2a344ba38905d6fabe487918542e8165a37281
khadija-mahdi <khadijamahdi6@gmail.com>
2023-05-22 22:22:08 +0100

    hello

--- philo/main.c
+++ philo/main.c
@@ -6,21 +6,43 @@
 /*   By: kmahdi <kmahdi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 16:14:58 by kmahdi            #+#    #+#             */
-/*   Updated: 2023/05/16 03:58:25 by kmahdi           ###   ########.fr       */
+/*   Updated: 2023/05/22 22:21:14 by kmahdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+
+long running_time(struct timeval start_time, struct timeval current_time) 
+{
+    gettimeofday(&current_time, NULL);
+    long seconds = current_time.tv_sec - start_time.tv_sec;
+    long microseconds = current_time.tv_usec - start_time.tv_usec;
+    if (microseconds < 0)
+	{
+        seconds--;
+        microseconds += 1000000;
+    }
+    long total_microseconds = (seconds * 1000000) + microseconds;
+	return (total_microseconds);
+}
+
 void	*philo_created(void *arg)
 {
 	t_data			*data;
+	struct timeval start_time, current_time;
 
+    gettimeofday(&start_time, NULL);
 	data = (t_data *) arg;
-	printf(" philosopher number %d has taken a fork\n",data->args->philo_nbr);
-	pthread_mutex_lock(data->forks);
-	usleep(data->args->eat_time * 1000);
-	pthread_mutex_unlock(data->forks);
+	while (1)
+	{
+		sleep(1);
+		long total_microseconds = running_time(start_time, current_time);
+		pthread_mutex_lock(data->forks);
+		printf(" %ld philosopher number %d has taken a fork\n",total_microseconds, data->args->philo_nbr);
+		usleep(data->args->eat_time);
+		pthread_mutex_unlock(data->forks);
+	}
 	return (NULL);
 }
 
@@ -62,26 +84,3 @@ int	main(int argc, char **argv)
 	data->philosophers = create_philosophers(data);
 	data->forks = create_forks (data);
 }
-
-
-// void	*philo_created(void *arg)
-// {
-// 	t_data			*data;
-// 	struct timeval	start_time;
-// 	struct timeval	end_time;
-// 	long			elapsed_time;
-
-// 	gettimeofday(&start_time, NULL);
-// 	data = (t_data *) arg;
-// 	printf(" %ld philosopher number %d has taken a fork\n", elapsed_time ,data->args->philo_nbr);
-// 	pthread_mutex_lock(data->forks);
-// 	printf(" %ld philosopher number %d has taken a fork\n", elapsed_time ,data->args->philo_nbr);
-// 	usleep(data->args->eat_time * 1000);
-// 	pthread_mutex_unlock(data->forks);
-// 	gettimeofday(&end_time, NULL);
-//     elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1000;
-//     elapsed_time += (end_time.tv_usec - start_time.tv_usec) / 1000;
-// 	printf(" %ld philosopher number %d has taken a fork\n", elapsed_time ,data->args->philo_nbr);
-//     // printf("Elapsed time: %ld ms\n", elapsed_time);
-// 	return (NULL);
-// }
